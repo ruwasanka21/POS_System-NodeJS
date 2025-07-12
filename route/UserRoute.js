8931955de5f7bec6d74dbc8b7628f78431f614e5
ruwasanka21 <ruwasanka21@gmail.com>
2025-07-12 14:07:20 +0530

    1

---
+++ route/UserRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const UserController = require('../controller/userController');
+
+const router = express.Router();
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+
+module.exports = router;
